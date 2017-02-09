b0f11f78ca5966945eb773d23dc3db4ff5569adf
Marcel Teruo Nakamine <marcel.nakamine@outlook.com>
2017-02-09 19:50:41 -0200

    Hello, World in Express framework

---
+++ server.js
@@ -0,0 +1,8 @@
+const configureExpress = require('./config/express');
+
+let port = 4040;
+const app = configureExpress();
+app.listen(port);
+module.exports = app;
+
+console.log(`Server running at http://localhost:${port}`);
