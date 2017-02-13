def6b04993f07886b8e5388a7c0e68898ed69639
Marcel Nakamine <marcel.nakamine@outlook.com>
2017-02-13 00:18:44 -0200

    configuring the mongoose

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+const configureMongoose = require('./config/mongoose');
 const configureExpress = require('./config/express');
 
 let port = 4040;
+const db = configureMongoose();
 const app = configureExpress();
 app.listen(port);
 module.exports = app;
