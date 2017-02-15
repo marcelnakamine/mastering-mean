add6da0d386d3f3ab78a2ee80e5bfdc46b3e750a
Marcel Teruo Nakamine <marcel.nakamine@outlook.com>
2017-02-15 19:44:16 -0200

    Set the UserSchema

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const configureMongoose = require('./config/mongoose');
 const configureExpress = require('./config/express');
 
 let port = 4040;
+// Make sure that Mongoose configuration file is loaded before any other configuration is performed
+// This is important since any module that is loaded after this module will be able to use the User model without loading it by itself.
 const db = configureMongoose();
 const app = configureExpress();
 app.listen(port);
