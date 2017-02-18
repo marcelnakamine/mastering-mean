9be754133506abc25e2cfb416d9af6b88292fb7f
Marcel Nakamine <marcel.nakamine@outlook.com>
2017-02-18 18:59:48 -0200

    extracted generic methods to helper for mongoose

---
+++ app/models/helper.mongoose.js
@@ -0,0 +1,27 @@
+exports.toLower = function (v) {
+  return v.toLowerCase();
+};
+
+exports.toObfuscate = function (pass) {
+  if (!pass) {
+    return pass;
+  } else {
+    let passParsed = "";
+    for (var i = 0; i < pass.length; i++) {
+      passParsed += '*';
+    }
+    return passParsed;
+  }
+};
+
+exports.fixUrl = function(url) {
+  if (!url) {
+    return url;
+  } else {
+    if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+      url = 'http://' + url;
+    }
+
+    return url;
+  }
+};
--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const helper = require('./helper.mongoose');
 
 const UserSchema = new Schema({
   firstName: String,
   lastName: String,
-  email: String,
-  username: String,
+  email: {
+    type: String,
+    set: helper.toLower
+  },
+  username: {
+    type: String,
+    set: helper.toLower,
+    trim: true
+  },
   password: {
     type: String,
-    trim: true
+    get: helper.toObfuscate
   },
   created: {
     type: Date,
@@ -16,18 +24,11 @@ const UserSchema = new Schema({
   },
   website: {
     type: String,
-    set: function(url) {
-      if (!url) {
-        return url;
-      } else {
-        if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
-          url = 'http://' + url;
-        }
-
-        return url;
-      }
-    }
+    set: helper.fixUrl
   },
 });
+
+//This will force Mongoose to include getters when converting the MongoDB document to a JSON representation and will allow the output of documents using res.json() in order to include the getter's behavior. If you didn't include this, you would have your document's JSON representation ignoring the getter modifiers.
+UserSchema.set('toJSON', {getters: true})
 
 mongoose.model('User', UserSchema);
