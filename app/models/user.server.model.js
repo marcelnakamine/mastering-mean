1eed6bb674bd05e2154a4e73c6b4ba5cf6621f77
Marcel Nakamine <marcel.nakamine@outlook.com>
2017-02-18 16:53:13 -0200

    custom setter modifier for website

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -13,7 +13,21 @@ const UserSchema = new Schema({
   created: {
     type: Date,
     default: Date.now
-  }
+  },
+  website: {
+    type: String,
+    set: function(url) {
+      if (!url) {
+        return url;
+      } else {
+        if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {
+          url = 'http://' + url;
+        }
+
+        return url;
+      }
+    }
+  },
 });
 
 mongoose.model('User', UserSchema);
